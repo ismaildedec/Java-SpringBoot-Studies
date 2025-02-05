2be7d360928f7a2593e32ce7434d7e3a13d8118e
ismaildedec <ddcismail@gmail.com>
2025-02-05 17:54:59 +0300

    Reading a File with Checked Exceptions

--- exceptionDemo/src/exceptionDemo/Main.java
+++ exceptionDemo/src/exceptionDemo/Main.java
@@ -1,26 +1,46 @@
 package exceptionDemo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		
+		/*
+		 * try {
+		 * 
+		 * int[] sayilar = new int[] {1,2,3,4}; System.out.println(sayilar[5]);
+		 * 
+		 * } catch (ArrayIndexOutOfBoundsException exception) {
+		 * 
+		 * System.out.println(exception); }catch (StringIndexOutOfBoundsException
+		 * exception) {
+		 * 
+		 * System.out.println(exception); }catch (Exception exception) {
+		 * 
+		 * System.out.println("Loglandı: "+ exception); }finally {
+		 * System.out.println("Ben her türlü çalışırım"); }
+		 */
+		
+		BufferedReader bufferedReader = null;
+		int total = 0;
+		
 		try {
 			
-			int[] sayilar = new int[] {1,2,3,4};
-			System.out.println(sayilar[5]);
+			 bufferedReader = new BufferedReader(new FileReader("C:\\Users\\İsmail\\eclipse-workspace\\exceptionDemo\\src\\exceptionDemo\\sayilar.txt"));
+			String line = null;
 			
-		} catch (ArrayIndexOutOfBoundsException exception) {
+			while ( (line = bufferedReader.readLine()) != null) {
+				
+				total += Integer.valueOf(line);
+			}
+			 System.out.println("Toplam: " + total);
+			 
+		} catch (Exception e) {
 			
-			System.out.println(exception);
-		}catch (StringIndexOutOfBoundsException exception) {
-			
-			System.out.println(exception);
-		}catch (Exception exception) {
-			
-			System.out.println("Loglandı: "+ exception);
-		}finally {
-			System.out.println("Ben her türlü çalışırım");
+			e.printStackTrace();
 		}
 		
 
