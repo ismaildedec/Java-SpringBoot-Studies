ecd56c5e84e4e21c6287e7f8c13562677c738010
ismaildedec <ddcismail@gmail.com>
2025-03-06 01:51:07 +0300

    @Query annotation

--- spring-data-jpa/src/main/java/com/ihsandedec/repository/IStudentRepository.java
+++ spring-data-jpa/src/main/java/com/ihsandedec/repository/IStudentRepository.java
@@ -1,11 +1,27 @@
 package com.ihsandedec.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ihsandedec.entites.Student;
 
 public interface IStudentRepository extends JpaRepository<Student, Integer>{
 	
 	//Buraya kendi özel metodlarımızı yazıyoruz
+	
+	//HQL : Sınıfın ismi ve değişken isimleri kullanılarak sorgular yazılır 
+	//@Query(value = "from Student" , nativeQuery = false)
+	
+	//SQL: Tablo ismi ve tablo içerisindeki değişken isimleri kullanılır.
+	@Query(value = "select * from student.student" , nativeQuery = true)
+	List<Student> findAllStudents();
+	
+	@Query(value = "from Student s WHERE s.id= :studentId")
+	Optional<Student> findStudentById(Integer studentId);
+	
+	
 
 }
